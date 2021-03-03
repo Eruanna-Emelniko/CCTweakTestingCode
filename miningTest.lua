75f024c8ff57a171e45310c898b5c6a6a92e5796
Mug-Wyllow <nathan.vollmers17@gmail.com>
2021-03-03 11:43:14 -0600

    --test, Just testing the them
    --miningTest, First plane coded, fixed problem with the end of row 2

--- miningTest.lua
+++ miningTest.lua
@@ -1,1 +1,25 @@
-turtle.dig()
+function digForward()
+    turtle.dig()
+    turtle.forward()
+    --digs infront of the turtle then moves in   
+end
+
+while turtle.getFuelLevel > 500 do
+    digForward()
+    turtle.turnRight()
+    digForward()
+    digForward()
+    turtle.digDown()
+    turtle.down()
+    turtle.turnRight()
+    turtle.turnRight()
+    digForward()
+    digForward()
+    turtle.digDown()
+    turtle.turnLeft()
+    turtle.turnLeft()
+    turtle.down()
+    digForward()
+    digForward()
+    --Row one dig, 3X3, starts in top left, ends in botton right
+end
