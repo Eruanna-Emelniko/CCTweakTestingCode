7b48dd0d90ac96fd21f28fc02e6166e415fc9abf
Mug-Wyllow <nathan.vollmers17@gmail.com>
2021-03-03 12:16:34 -0600

    --added the needed barkets the main lop -- started refuel lop

--- miningTest.lua
+++ miningTest.lua
@@ -1,23 +1,23 @@
 function digForward()
     turtle.dig()
     turtle.forward()
-    --digs infront of the turtle then moves in   
+--digs infront of the turtle then moves in   
 end
 
 function digUp()
     turtle.digUp()
     turtle.up()
-    --digs up then moves into the empty space it made
+--digs up then moves into the empty space it made
 end
 
 function didAFull180()
     turtle.turnLeft()
     turtle.turnLeft()
-    --turns the turtle around, AND saves on code.
+--turns the turtle around, AND saves on code.
     
 end
 
-while turtle.getFuelLevel > 500 do
+while turtle.getFuelLevel() > 500 do
     digForward()
     turtle.turnRight()
     digForward()
@@ -48,4 +48,9 @@ while turtle.getFuelLevel > 500 do
     digForward()
     turtle.turnLeft()
 --Row two dig, 3X3, starts botton right, ends top left, zig-zags
+end
+
+if turtle.getFuelLevel() < 500 then
+    slot = turtle.getSelectedSlot()
+
 end
